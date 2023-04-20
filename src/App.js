4045dc91c5f1de7560bb20cd35a0c94e2ec61e93
Rishi Raj Jain <rjain@llnw.com>
2023-04-20 22:29:53 +0530

    remove sw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Layer0RUM from './rum'
-import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
-import { install } from '@edgio/prefetch/window'
-import installDevtools from '@edgio/devtools/install'
 
 // Include the RUM Analytics in the production build only
 if (process.env.NODE_ENV === 'production') {
@@ -11,12 +8,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const App = () => {
-  useEffect(() => {
-    // Enable service worker inside the window
-    install()
-    // Enable devtools manually, instead of relying on defaults by Edgio
-    installDevtools()
-  }, [])
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#9a1ab1] via-[#004966] to-[#01B18D]">
       <Navbar />
